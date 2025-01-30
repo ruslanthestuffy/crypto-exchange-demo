1a900d48f2927a90f8e70a6debd9913ee254d141
ruslanthestuffy <bigjohnnyscript@gmail.com>
2025-01-30 22:28:42 +0800

    feat(main): fix env

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'node:path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_COINMARKETCAP_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(() => {
+  const env = loadEnv('', process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_COINMARKETCAP_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@entities': path.resolve(__dirname, 'src/entities'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@shared': path.resolve(__dirname, 'src/shared'),
-      '@widgets': path.resolve(__dirname, 'src/widgets'),
+    resolve: {
+      alias: {
+        '@app': path.resolve(__dirname, 'src/app'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@entities': path.resolve(__dirname, 'src/entities'),
+        '@features': path.resolve(__dirname, 'src/features'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@shared': path.resolve(__dirname, 'src/shared'),
+        '@widgets': path.resolve(__dirname, 'src/widgets'),
+      },
     },
-  },
+  };
 });
